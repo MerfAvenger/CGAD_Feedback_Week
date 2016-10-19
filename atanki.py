55fbb089f2e41f39a0d426547b47b31a4616c191
MerfAvenger <connor.barthelmie@gmail.com>
2016-10-19 14:02:07 +0100

    Modified Wormy file to start Atanki
    Began pulling code from wormy to leave basic graphics and functionality for Atanki.

---
+++ atanki.py
@@ -0,0 +1,7 @@
+# Main file for Atanki, the Atari tank game knockoff.
+# Assembled using base code from wormy.py.
+
+# Worked on by Adam Campbell, Rory Kerr and Connor Barthelmie.
+
+import random, pygame, sys
+from pygame.locals import *
