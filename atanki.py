a0ef1221270c7d32f013017c466d5aab1324f7f5
MerfAvenger <connor.barthelmie@gmail.com>
2016-10-21 13:05:15 +0100

    Updated Atanki.py with additional features
    Adam and Connor's upgrades committed to Atanki.py

--- atanki.py
+++ atanki.py
@@ -3,15 +3,15 @@
 
 # Worked on by Adam Campbell, Rory Kerr and Connor Barthelmie.
 
-import random, pygame, sys,
+import random, pygame, sys
 from pygame.locals import *
 
 # -----------------------------------------------------------------------------------------------------------------------------------------
 # GRAPHICS SETUP
 
 FPS = 20
-WINDOWWIDTH = 1600
-WINDOWHEIGHT = 900
+WINDOWWIDTH = 900
+WINDOWHEIGHT = 460
 CELLSIZE = 20
 assert WINDOWWIDTH % CELLSIZE == 0, "Window width must be a multiple of cell size."
 assert WINDOWHEIGHT % CELLSIZE == 0, "Window height must be a multiple of cell size."
@@ -26,6 +26,77 @@ GREEN     = (  0, 255,   0)
 DARKGREEN = (  0, 155,   0)
 DARKGRAY  = ( 40,  40,  40)
 BGCOLOR = GREEN
+################################################
+##CREATING ARRAY SYSTEM (ANNOYING AS SHIT)
+width = 45
+height =  45
+Matrix = [[0 for x in range(width)] for y in range(height)] 
+
+##DRAWING THE MAP
+for height in range (0,45):
+    for width in range (0,45):
+        if width ==0:
+            Matrix[width][height] = 1
+        elif width ==44:
+            Matrix[width][height] = 1
+        elif height ==0:
+            Matrix[width][height] = 1
+        elif height ==22:
+            Matrix[width][height] = 1
+        else:
+            Matrix[width][height] = 0
+            
+            
+            
+
+for i in range (4,10):
+    Matrix[4][i]  = 1
+    Matrix[4][(i+9)] = 1
+    
+    Matrix[40][i] = 1
+    Matrix[40][i+9] = 1
+    
+    
+for x in range (5,13):
+        
+    Matrix[x][4] = 1
+    Matrix[x][18] = 1
+    
+    Matrix[x+27][4] = 1
+    Matrix[x+27][18] = 1
+    
+for x in range (8,13):
+
+    Matrix[x+12][7]=1
+    Matrix[x+12][15]=1
+    
+    for y in range(8,15):
+        Matrix[x][y]=1
+        Matrix[x+24][y]=1
+
+
+for i in range (4,8):
+    Matrix[16][i]  = 1
+    Matrix[16][(i+11)] = 1
+    
+    Matrix[28][i] = 1
+    Matrix[28][i+11] = 1
+    
+for y in range(1,8):
+    Matrix[20][y] = 1
+    Matrix[24][y] = 1
+    
+    Matrix[20][y+14] = 1
+    Matrix[24][y+14] = 1
+    
+for x in range(16,29):
+    Matrix[x][11] = 1
+   
+    
+
+gPlayer_X = 2
+gPlayer_Y = 2
+Matrix[gPlayer_X][gPlayer_Y] = 2
 
 # ----------------------------------------------------------------------------------------------------------------------------------------
 
@@ -47,8 +118,10 @@ LEFT = 'left'
 RIGHT = 'right'
 
 def main():
+
     init()
-    showStartScreen()
+    drawMap()
+    drawGrid()
     while True:
         runGame()
         showGameOverScreen()
@@ -72,8 +145,10 @@ def game_init():
     gDirection = UP
     
 def runGame():
+    
     game_init()    
     while True: # main game loop
+        Matrix[gPlayer_X][gPlayer_Y] = 2
         game_over = game_update()
         game_render()
         if game_over == True:
@@ -81,32 +156,85 @@ def runGame():
 # ------------------------------------------------------------------------------------------------------------------------------------------
 # ALMOST ALL NEW CODE WILL PROBABLY GO IN GAME_UPDATE. ANYTHING RELEVANT TO NON-SETUP HERE.                   
                    
-def game_update():
-    global gWormCoords, gDirection, gApple
+def drawGrid():
 
+    for x in range(0, WINDOWWIDTH, CELLSIZE): # draw vertical lines
+        pygame.draw.line(DISPLAYSURF, DARKGRAY, (x, 0), (x, WINDOWHEIGHT))
+    for y in range(0, WINDOWHEIGHT, CELLSIZE): # draw horizontal lines
+        pygame.draw.line(DISPLAYSURF, DARKGRAY, (0, y), (WINDOWWIDTH, y))
+        
+    
+def game_update():
+
+    global gPlayer_X, gPlayer_Y
+    global turretDir, playerShot.x, playerShot.y, playerShot.d, shotConnected = false, shotFired = false
+
+    
+    #MOVEMENT OF TANK SEGMENT
     for event in pygame.event.get(): # event handling loop
         if event.type == QUIT:
             terminate()
         elif event.type == KEYDOWN:
-            if (event.key == K_LEFT or event.key == K_a) and gDirection != RIGHT:
-                gDirection = LEFT
-            elif (event.key == K_RIGHT or event.key == K_d) and gDirection != LEFT:
-                gDirection = RIGHT
-            elif (event.key == K_UP or event.key == K_w) and gDirection != DOWN:
-                gDirection = UP
-            elif (event.key == K_DOWN or event.key == K_s) and gDirection != UP:
-                gDirection = DOWN
+            if (event.key == K_LEFT or event.key == K_a):
+                if Matrix[(gPlayer_X-1)][gPlayer_Y]!=1:
+                    Matrix[gPlayer_X][gPlayer_Y] = 0
+                    gPlayer_X -= 1
+                    direction = LEFT
+            elif (event.key == K_RIGHT or event.key == K_d):
+                if Matrix[(gPlayer_X+1)][gPlayer_Y]!=1:
+                    Matrix[gPlayer_X][gPlayer_Y] = 0
+                    gPlayer_X += 1
+                    direction = RIGHT
+            elif (event.key == K_UP or event.key == K_w):
+                if Matrix[(gPlayer_X)][gPlayer_Y-1]!=1:
+                    Matrix[gPlayer_X][gPlayer_Y] = 0
+                    gPlayer_Y -= 1
+                    direction = UP
+            elif (event.key == K_DOWN or event.key == K_s):
+                if Matrix[(gPlayer_X)][gPlayer_Y+1]!=1:
+                    Matrix[gPlayer_X][gPlayer_Y] = 0
+                    gPlayer_Y += 1
+                    direction = DOWN                        
             elif event.key == K_ESCAPE:
-                terminate()
+                terminate()       
+                
+        #MOVING AND ROTATION OF TURRET SEGMENT    
+        elif event.type == KEYDOWN:
+            if (event.key == K_LEFT):
+                playerShot = shoot(player.x, player.y, turretDir) 
+                turretDir = LEFT
+                shotFired = true
+            elif (event.key == K_RIGHT):
+                playerShot = shoot(player.x, player.y, turretDir) 
+                turretDir = RIGHT
+                shotFired = true
+            elif (event.key == K_UP):
+                playerShot = shoot(player.x, player.y, turretDir) 
+                turretDir = UP
+                shotFired = true
+            elif (event.key == K_DOWN or event.key == K_s):
+                playerShot = shoot(player.x, player.y, turretDir) 
+                turretDir = DOWN
+                shotFired = true
+        
+        #If the shot is fired but hasn't hit anything yet move it along in the appropriate direction
+        if (shotFired and shotConnected != true)
+            if (playerShot.d == UP and Matrix[(playerShot.x)][playerShot.y+1]!=1)
+                playerShot.y+1
+            elif (playerShot.d == DOWN and Matrix[(playerShot.x)][playerShot.y-1]!=1)
+                playerShot.y-1
+            elif (playerShot.d == LEFT and Matrix[(playerShot.x-1)][playerShot.y]!=1)
+                playerShot.x-1
+            elif (playerShot.d == RIGHT and Matrix[(playerShot.x+1)][playerShot.y]!=1)
+                playerShot.x+1
+            else 
+                shotConnected = true
                    
 # ------------------------------------------------------------------------------------------------------------------------------------------
                    
 def game_render():
-    DISPLAYSURF.fill(BGCOLOR)
+    drawMap()
     drawGrid()
-    drawWorm(gWormCoords)
-    drawApple(gApple)
-    drawScore(len(gWormCoords) - 3)
     pygame.display.update()
     FPSCLOCK.tick(FPS)
                    
@@ -161,10 +289,47 @@ def showGameOverScreen():
                    
 def drawGrid():
     for x in range(0, WINDOWWIDTH, CELLSIZE): # draw vertical lines
-        pygame.draw.line(DISPLAYSURF, DARKGRAY, (x, 0), (x, WINDOWHEIGHT))
+        pygame.draw.line(DISPLAYSURF, BLACK, (x, 0), (x, WINDOWHEIGHT))
     for y in range(0, WINDOWHEIGHT, CELLSIZE): # draw horizontal lines
-        pygame.draw.line(DISPLAYSURF, DARKGRAY, (0, y), (WINDOWWIDTH, y))
+        pygame.draw.line(DISPLAYSURF, BLACK, (0, y), (WINDOWWIDTH, y))
 
+def drawMap():
 
+    for y in range(0, 45):
+        for x in range (0, 45):
+            if Matrix[x][y] == 0:
+                WallRect = pygame.Rect(x*CELLSIZE, y*CELLSIZE, CELLSIZE, CELLSIZE)
+                pygame.draw.rect(DISPLAYSURF, DARKGRAY, WallRect)
+            elif Matrix[x][y] == 1:
+                FloorRect = pygame.Rect(x*CELLSIZE, y*CELLSIZE, CELLSIZE, CELLSIZE)
+                pygame.draw.rect(DISPLAYSURF, RED, FloorRect)
+            elif Matrix[x][y] == 2:
+                PlayerRect = pygame.Rect(x*CELLSIZE, y*CELLSIZE, CELLSIZE, CELLSIZE)
+                pygame.draw.rect(DISPLAYSURF, GREEN, PlayerRect)
+                
+                
+     
+def shoot(player.x, player.y, direction)
+{
+    if(direction == 'UP')
+        bullet.x = player.x
+        bullet.y = player.y+1
+        bullet.d = UP
+    if(direction == 'DOWN')
+        bullet.x = player.x
+        bullet.y = player.y-1
+        bullet.d = DOWN
+    if(direction == 'LEFT')
+        bullet.x = player.x-1
+        bullet.y = player.y    
+        bullet.d = LEFT
+    if(direction == 'RIGHT')
+        bullet.x = player.x+1
+        bullet.y = player.y
+        bullet.d = RIGHT
+    
+    return bullet.x, bullet.y, bullet.d
+}
+            
 if __name__ == '__main__':
     main()
